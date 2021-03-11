9585d5542704df07442179d89ee5cd5e69685155
mihalin <stopa3.10.1996@mail.ru>
2021-03-11 14:36:11 +0300

    Windows compatibility

--- scripts/update-po-files.sh
+++ scripts/update-po-files.sh
@@ -1,2 +1,2 @@
-pygettext3 -d en -o locale/en_US/LC_MESSAGES/en.pot data/*.py
+xgettext -d en -o locale/en_US/LC_MESSAGES/en.pot data/*.py
 msgmerge locale/en_US/LC_MESSAGES/en.po locale/en_US/LC_MESSAGES/en.pot --update
