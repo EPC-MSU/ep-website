7f88960cc5933e3fe8e8f11c241f23182778c9e9
vladbelov <skyllann7@gmail.com>
2021-06-17 14:06:11 +0300

    update .po and script

--- scripts/update-po-files.bat
+++ scripts/update-po-files.bat
@@ -1,2 +1,2 @@
-xgettext -d en -o locale/en_US/LC_MESSAGES/en.pot data/download.py data/other.py data/products.py --from-code=utf-8
-msgmerge locale/en_US/LC_MESSAGES/en.po locale/en_US/LC_MESSAGES/en.pot --update
+xgettext -d en -o ../locale/en_US/LC_MESSAGES/en.pot data/download.py ../data/other.py data/products.py --from-code=utf-8
+msgmerge ../locale/en_US/LC_MESSAGES/en.po ../locale/en_US/LC_MESSAGES/en.pot --update
