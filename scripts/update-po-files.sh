b3ed6d934c3d2f0cf4049b252dc6d63bd048b854
Andrey Marakulin <annndruha.github@gmail.com>
2024-04-08 13:10:52 +0300

    update scripts

--- scripts/update-po-files.sh
+++ scripts/update-po-files.sh
@@ -1,2 +1,16 @@
-xgettext -d en -o ../locale/en_US/LC_MESSAGES/en.pot ../data/download.py ../data/other.py ../data/products.py --from-code=utf-8
-msgmerge ../locale/en_US/LC_MESSAGES/en.po ../locale/en_US/LC_MESSAGES/en.pot --update
+# general + eyepoint
+xgettext -d en -o ../sites/eyepoint/locale/en_US/LC_MESSAGES/en.pot \
+  ../site_engine/specification/general.py \
+  ../sites/eyepoint/data/download.py ../sites/eyepoint/data/other.py ../sites/eyepoint/data/products.py \
+  --from-code=utf-8
+
+msgmerge ../sites/eyepoint/locale/en_US/LC_MESSAGES/en.po ../sites/eyepoint/locale/en_US/LC_MESSAGES/en.pot --update
+
+# general + usbadc10
+xgettext -d en -o ../sites/usbadc10/locale/en_US/LC_MESSAGES/en.pot \
+  ../site_engine/specification/general.py \
+  ../sites/usbadc10/data/download.py ../sites/usbadc10/data/other.py ../sites/usbadc10/data/products.py \
+  --from-code=utf-8
+
+msgmerge ../sites/usbadc10/locale/en_US/LC_MESSAGES/en.po ../sites/usbadc10/locale/en_US/LC_MESSAGES/en.pot --update
+pause
